286e077bb7aa3544a4aa7f146f4ef93d8b469c3c
Sumati A Gadagi <134225744+swatigadagi@users.noreply.github.com>
2026-05-29 01:38:59 +0530

    Update app.js

--- Frontend/app.js
+++ Frontend/app.js
@@ -1,19 +1,17 @@
-const API_URL = "http://localhost:8080/api/health";
 const API_URL = process.env.API_URL;
-fetch(API_URL)
-  .then(response => response.json())
-  .then(data => {
-
+fetch(`${API_URL}/api/v1/health`)
+  .then((response) => response.json())
+  .then((data) => {
     document.getElementById("message").innerHTML = `
       <h3>Application Status</h3>
       <p><strong>Status:</strong> ${data.status}</p>
       <p><strong>Message:</strong> ${data.message}</p>
       <p><strong>Time:</strong> ${data.timestamp}</p>
     `;
+    console.log(data);
   })
-  .catch(error => {
+  .catch((error) => {
     console.error(error);
-
     document.getElementById("message").innerHTML = `
       <p>Backend connection failed</p>
     `;
