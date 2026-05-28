47c93b7afec91ef93139d5fe798edc23b871b1be
Sumati A Gadagi <134225744+swatigadagi@users.noreply.github.com>
2026-05-29 02:04:32 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -17,7 +17,9 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: false
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 app.get("/", (req, res) => {
